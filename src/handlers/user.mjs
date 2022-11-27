f9a31b6a3053049b176cb46b00db344af5c7bbe6
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-11-27 13:22:16 -0500

    Add token validation for site routes

--- src/handlers/user.mjs
+++ src/handlers/user.mjs
@@ -1,3 +1,46 @@
+import { prisma } from "../db.mjs";
 import { hashPassword, comparePassword, createJWT } from "../modules/auth.mjs";
-export const signupUser = (req, res) => {};
-export const signinUser = (req, res) => {};
+
+export const signupUser = async (req, res) => {
+  const hash = await hashPassword(req.body.password);
+  const user = await prisma.user.create({
+    data: {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      password: hash,
+      email: req.body.email,
+      phone: req.body.phone,
+      country: req.body.country,
+      state: req.body.state,
+      city: req.body.city,
+      address: req.body.address,
+    },
+  });
+  const token = createJWT(user);
+  res.json({
+    data: {
+      token: token,
+    },
+  });
+};
+export const signinUser = async (req, res) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      username: req.body.username,
+    },
+  });
+
+  const isPasswordValid = await comparePassword(
+    req.body.password,
+    user.password
+  );
+  if (!isPasswordValid) {
+    res.status(401);
+    res.json({ message: "Invalid username or password" });
+    return;
+  } else {
+    const token = createJWT(user);
+    res.json({ data: { token: token } });
+  }
+};
--- src/server.mjs
+++ src/server.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./router.mjs";
-import { signupUser, signinUser } from "./handlers/user.mjs"
+import { signupUser, signinUser } from "./handlers/user.mjs";
+import { validateToken } from "./modules/auth.mjs";
 
 const app = express();
 app.use(express.json());
@@ -10,7 +11,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Personal IoT app" });
 });
 
-app.use("/api", router);
+app.use("/api", validateToken, router);
 app.post("/signup", signupUser);
 app.post("/signin", signinUser);
 
